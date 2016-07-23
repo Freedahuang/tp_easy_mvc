642e33bf7e2b7db705d5f0c3c4f22baa74a9958a
huangfohai <695772693@qq.com>
2016-07-23 17:43:32 +0800

    sdf

--- Home/Model/IndexModel.class.php
+++ Home/Model/IndexModel.class.php
@@ -1,10 +1,10 @@
-<?php
+﻿<?php
 	
 	//判断当前请求是否正常，如果不是通过index.php进行访问的，就退出系统
 	if(!defined('ACCESS')) die('Hacking');
 
 	/*
-	 * 首页模型类
+	 * 
 	 */
 	class IndexModel extends Model{
 		public function __construct($tableName){
