2b9d84018865b33ef9fe7b4746e9d0df5ec58607
huangfohai <695772693@qq.com>
2016-07-23 17:41:26 +0800

    sdf

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php
+﻿<?php
 	//项目入口
 	
 	//为了能保证用户是从这index.php这个入口进去的，一般会在这个入口增加一个常量，
@@ -9,7 +9,7 @@
 	
 	require_once './Core/Application.class.php';
 	
-	//对系统进行初始化
+	//对系
 	
 	Application::run();
 	
